862a1775e1622c46a95ddccb6abf0748f0945a9b
Bun Camara <bun.camara@cps.gov.uk>
2025-08-15 12:51:44 +0100

    new asp

--- modules/app-service-plan/main.tf
+++ modules/app-service-plan/main.tf
@@ -1,11 +1,7 @@
-resource "azurerm_app_service_plan" "service_plan" {
+resource "azurerm_service_plan" "service_plan" {
   name                = var.asp_name
-  location            = var.location
   resource_group_name = var.main_rg_name
-  kind                = "Linux"
-
-  sku {
-    tier = "Basic"
-    size = "B2"
-  }
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "B2"
 }
