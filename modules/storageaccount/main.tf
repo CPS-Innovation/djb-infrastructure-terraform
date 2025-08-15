26fc8aec7e4955d87f026a572d73d96cefaf49d7
Bun Camara <bun.camara@cps.gov.uk>
2025-08-15 15:23:42 +0100

    sa

--- modules/storageaccount/main.tf
+++ modules/storageaccount/main.tf
@@ -13,26 +13,3 @@ resource "azurerm_storage_account" "sa" {
     environment = var.environment
   }
 }
-
-resource "azurerm_private_endpoint" "sa" {
-  name                = "pe-djb-${var.environment}"
-  location            = var.location
-  resource_group_name = var.main_rg_name
-  subnet_id           = var.subnet_id
-
-  private_service_connection {
-    name                           = azurerm_storage_account.sa.name
-    private_connection_resource_id = azurerm_storage_account.sa.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "sa-dns-zone-group"
-    private_dns_zone_ids = [var.private_dns_zone_ids]
-  }
-
-  tags = {
-    environment = var.environment
-  }
-}
--- modules/storageaccount/variables.tf
+++ modules/storageaccount/variables.tf
@@ -12,13 +12,3 @@ variable "main_rg_name" {
   type        = string
   description = "The name of the resource group in which to create the subnet"
 }
-
-variable "subnet_id" {
-  type        = string
-  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint. Changing this forces a new resource to be created"
-}
-
-variable "private_dns_zone_ids" {
-  type        = string
-  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group"
-}
