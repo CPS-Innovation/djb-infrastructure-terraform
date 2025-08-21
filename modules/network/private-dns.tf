3cf0f6bd2b964dc5e58c4424583064072cc70d34
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-08-21 15:39:23 +0100

    make dns-vnet link resource dependent of the private dns zone resource

--- modules/network/private-dns.tf
+++ modules/network/private-dns.tf
@@ -11,4 +11,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns" {
   resource_group_name   = var.vnet_rg
   private_dns_zone_name = each.value
   virtual_network_id    = var.virtual_network_id
+
+  depends_on = [azurerm_private_dns_zone.dns]
 }
