4a3eb0e722350bbf933cde17ff96f1e1f481af68
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-08-22 11:41:06 +0100

    fix role assignment principal id input for fa

--- modules/function-app/iam.tf
+++ modules/function-app/iam.tf
@@ -3,5 +3,5 @@ resource "azurerm_role_assignment" "sa" {
 
   scope                = var.sa_id
   role_definition_name = each.value
-  principal_id         = azurerm_linux_function_app.fa.id
+  principal_id         = azurerm_linux_function_app.fa.identity[0].principal_id
 }
