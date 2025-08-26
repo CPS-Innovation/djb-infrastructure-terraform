f5a701c21af1cc3831647be46421f97b50dc9bfb
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-08-26 12:10:10 +0100

    disable public network access to monitoring resources

--- modules/app-insights/log-analytics.tf
+++ modules/app-insights/log-analytics.tf
@@ -1,9 +1,11 @@
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "log-analytics-djb-${var.environment}"
-  location            = var.location
-  resource_group_name = var.main_rg_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 60
+  name                       = "log-analytics-djb-${var.environment}"
+  location                   = var.location
+  resource_group_name        = var.main_rg_name
+  sku                        = "PerGB2018"
+  retention_in_days          = 60
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
 
   tags = var.tags
 }
