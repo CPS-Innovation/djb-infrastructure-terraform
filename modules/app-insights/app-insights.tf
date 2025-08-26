e928606914da311ac1cadb3663b35b0ffeb88a75
Lilach <Lilach.Davis@cps.gov.uk>
2025-08-26 14:20:15 +0100

    disable public network access to monitoring resources (#10)

--- modules/app-insights/app-insights.tf
+++ modules/app-insights/app-insights.tf
@@ -1,9 +1,11 @@
 resource "azurerm_application_insights" "ai" {
-  name                = "ai-djb-${var.environment}"
-  location            = var.location
-  resource_group_name = var.main_rg_name
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law.id
+  name                       = "ai-djb-${var.environment}"
+  location                   = var.location
+  resource_group_name        = var.main_rg_name
+  application_type           = "web"
+  workspace_id               = azurerm_log_analytics_workspace.law.id
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
 
   tags = var.tags
 }
