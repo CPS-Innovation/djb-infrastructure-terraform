c6c6bf9e5c170a510fb8c9665a12dc8e6c5de7c8
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-09-04 11:15:00 +0100

    disable disk encryption

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault" "kv" {
   name                        = var.kv_name
   location                    = var.location
   resource_group_name         = var.main_rg_name
-  enabled_for_disk_encryption = true
+  enabled_for_disk_encryption = false
   tenant_id                   = var.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = true
